c4f55c8f4a59bcc09b636f9a306163033b9062e2
notaarryan <aryanparmar2503@gmail.com>
2024-07-21 00:05:27 +0530

    js completed

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let button = document.querySelectorAll(".box");
+let box = document.querySelectorAll(".box");
+let startBtn = document.querySelector("#startButton");
+let resetBtn = document.querySelector("#resetButton");
+let menuBtn = document.querySelector("#menuButton");
+
 let winArr = [
   [0, 1, 2],
   [3, 4, 5],
@@ -14,26 +18,53 @@ let turn = "X";
 
 let win = false;
 
+const start = () => {
+  document.querySelector("#mainGame").style.display = "flex";
+  document.querySelector("#mainPage").style.display = "none";
+  box.forEach((val) => {
+    turn = "X";
+    val.disabled = false;
+    val.innerText = "";
+  });
+};
+
+const reset = () => {
+  turn = "X";
+  box.forEach((val) => {
+    val.innerText = "";
+  });
+};
+
+const menu = () => {
+  document.querySelector("#mainGame").style.display = "none";
+  document.querySelector("#mainPage").style.display = "flex";
+};
+
 const checkWin = () => {
   for (let arr of winArr) {
     console.log(
-      button[arr[0]].innerText,
-      button[arr[1]].innerText,
-      button[arr[2]].innerText
+      box[arr[0]].innerText,
+      box[arr[1]].innerText,
+      box[arr[2]].innerText
     );
     if (
-      button[arr[0]].innerText == button[arr[1]].innerText &&
-      button[arr[1]].innerText == button[arr[2]].innerText &&
-      button[arr[0]].innerText != "" &&
-      button[arr[1]].innerText != "" &&
-      button[arr[2]].innerText != ""
+      box[arr[0]].innerText == box[arr[1]].innerText &&
+      box[arr[1]].innerText == box[arr[2]].innerText &&
+      box[arr[0]].innerText != "" &&
+      box[arr[1]].innerText != "" &&
+      box[arr[2]].innerText != ""
     ) {
-      win = true;
+      document.querySelector("#endPage").style.display = "flex";
+      document.querySelector("#mainPage").style.display = "none";
+      document.querySelector("#mainGame").style.display = "none";
+      box.forEach((val) => {
+        val.disabled = true;
+      });
     }
   }
 };
 
-button.forEach((val) => {
+box.forEach((val) => {
   val.addEventListener("click", () => {
     if (turn === "X") {
       val.innerText = "X";
@@ -47,3 +78,7 @@ button.forEach((val) => {
   });
   val.addEventListener("click", checkWin);
 });
+
+startBtn.addEventListener("click", start);
+resetBtn.addEventListener("click", reset);
+menuBtn.addEventListener("click", menu);
